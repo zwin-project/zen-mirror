9ff98586e00b3eab2274301fe3a0a94c5e4a68ac
Aki-7 <aki.develop8128@gmail.com>
2022-09-16 23:55:39 +0900

    setup Boost

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,5 +1,9 @@
-#include <android_native_app_glue.h>
+#include "pch.h"
 
-void android_main(struct android_app *app) {
-    (void)app;
+void
+android_main(struct android_app *app)
+{
+  __android_log_print(
+      ANDROID_LOG_INFO, "ZEN", "Boost version %d", BOOST_VERSION);
+  (void)app;
 }
