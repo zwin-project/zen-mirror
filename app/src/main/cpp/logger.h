b57f7706059f7d892812364eaaaaaf216ab2e9e7
Aki-7 <aki.develop8128@gmail.com>
2022-09-18 16:54:45 +0900

    simple logger

---
+++ app/src/main/cpp/logger.h
@@ -0,0 +1,47 @@
+#pragma once
+
+namespace zen::display_system::oculus {
+
+struct ILogger {
+  // logger singleton set by InitializeLogger;
+  static std::unique_ptr<ILogger> instance;
+
+  typedef enum Severity {
+    DEBUG = 0,  // logs for debugging during development.
+    INFO,       // logs that may be useful to some users.
+    WARN,       // logs for recoverable failures.
+    ERROR,      // logs for unrecoverable failures.
+    FATAL,      // logs when aborting.
+    SILENT,     // for internal use only.
+  } Severity;
+
+  virtual ~ILogger() = default;
+
+  virtual void Print(Severity severity, const char* pretty_function,
+      const char* file, int line, const char* format, ...)
+      __attribute__((__format__(printf, 6, 7))) = 0;
+};
+
+#define LOG_DEBUG(format, ...)                                            \
+  ILogger::instance->Print(ILogger::DEBUG, __PRETTY_FUNCTION__, __FILE__, \
+      __LINE__, format, ##__VA_ARGS__)
+
+#define LOG_INFO(format, ...)                                            \
+  ILogger::instance->Print(ILogger::INFO, __PRETTY_FUNCTION__, __FILE__, \
+      __LINE__, format, ##__VA_ARGS__)
+
+#define LOG_WARN(format, ...)                                            \
+  ILogger::instance->Print(ILogger::WARN, __PRETTY_FUNCTION__, __FILE__, \
+      __LINE__, format, ##__VA_ARGS__)
+
+#define LOG_ERROR(format, ...)                                            \
+  ILogger::instance->Print(ILogger::ERROR, __PRETTY_FUNCTION__, __FILE__, \
+      __LINE__, format, ##__VA_ARGS__)
+
+#define LOG_FATAL(format, ...)                                            \
+  ILogger::instance->Print(ILogger::FATAL, __PRETTY_FUNCTION__, __FILE__, \
+      __LINE__, format, ##__VA_ARGS__)
+
+void InitializeLogger();
+
+}  // namespace zen::display_system::oculus
--- app/src/main/cpp/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-
-void
-android_main(struct android_app *app)
-{
-  __android_log_print(
-      ANDROID_LOG_INFO, "ZEN", "Boost version %d", BOOST_VERSION);
-  (void)app;
-}
